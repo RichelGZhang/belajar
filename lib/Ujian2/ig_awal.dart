daf1a689966ce4fb644cb3faae067271493bcf44
RichelGZhang <93985473+RichelGZhang@users.noreply.github.com>
2022-09-18 21:18:51 +0700

    Halaman search

--- lib/Ujian2/ig_awal.dart
+++ lib/Ujian2/ig_awal.dart
@@ -10,19 +10,21 @@ class IGAwal extends StatefulWidget {
 
 class _IGAwalState extends State<IGAwal> {
   String urlPanda = "https://asset.kompas.com/crops/ncgvDkq11ovx_624dxbv483x_iY=/0x0:648x432/750x500/data/photo/2021/10/05/615c371c61b81.jpg";
+  List<String> nama = ["Uby Jelek", "Byru Jelek", "Ybur Jelek"];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text("Instagram", style: TextStyle(
-                fontSize: 29,
-                fontWeight: FontWeight.bold,
-                fontFamily: "IG"
+                  fontSize: 29,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "IG"
               ),),
               Row(
                 children: [
@@ -44,41 +46,143 @@ class _IGAwalState extends State<IGAwal> {
         SizedBox(
           height: 5,
         ),
-        
+
         Container(
-          height: 80,
+          height: 90,
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
-              fotoKecil(urlPanda),
+              fotoKecil(urlPanda, nama[0]),
+              fotoKecil(urlPanda, nama[1]),
+              fotoKecil(urlPanda, nama[2]),
+              fotoKecil(urlPanda, nama[0]),
+              fotoKecil(urlPanda, nama[1]),
+              fotoKecil(urlPanda, nama[2]),
+              fotoKecil(urlPanda, nama[0]),
             ],
           ),
-        )
+        ),
+
+        Divider(height: 1, thickness: 1,),
+        SizedBox(height: 5,),
+
+        itemFoto(urlPanda, nama[0]),
+        itemFoto(urlPanda, nama[1]),
+        itemFoto(urlPanda, nama[2]),
+
       ],
     );
   }
 }
 
 
-Padding fotoKecil (String url){
+Padding fotoKecil (String url, String nama){
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 5),
-    child: Container(
-      width: 80,
-      height: 60,
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100)
-      ),
-      child: Image.network(url,
-        fit: BoxFit.cover,),
+    padding: const EdgeInsets.symmetric(horizontal: 8),
+    child: Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100)
+          ),
+          child: Image.network(url,
+            fit: BoxFit.cover,),
+        ),
+        SizedBox(height: 5,),
+        Text(nama)
+      ],
     ),
+  );
+}
+
+Column itemFoto(String url, String nama){
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 7),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 45,
+                        height: 45,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100)
+                        ),
+                        child: Image.network(url,
+                          fit: BoxFit.cover,),
+                      ),
+                      SizedBox(height: 5,),
+                    ],
+                  ),
+                ),
+                Text(nama),
+              ],
+            ),
+            Icon(MdiIcons.dotsVertical),
+          ],
+        ),
+      ),
+
+      Container(
+          height: 370,
+          child: Image.network(url, fit: BoxFit.cover,)
+      ),
+      Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Icon(MdiIcons.cardsHeartOutline, size: 30,),
+                SizedBox(width: 17,),
+                Icon(MdiIcons.chatProcessingOutline, size: 30,),
+                SizedBox(width: 17,),
+                Icon(MdiIcons.send, size: 30,),
+                SizedBox(width: 17,),
+              ],
+            ),
+            Icon(MdiIcons.bookmarkOutline, size: 30,),
+          ],
+        ),
+      ),
+
+      Container(
+        padding: EdgeInsets.only(left: 10),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("10 likes", style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),),
+            SizedBox(height: 7,),
+            Text(nama, style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),),
+            Text("Iceng adalah seekor kucing gepeng yang digeprek"),
+            Row(
+              children: [
+                Text("2 days ago", style: TextStyle(color: Colors.grey, fontSize: 12),),
+                Icon(MdiIcons.circleSmall),
+                Text("See translation", style: TextStyle(fontSize: 12),),
+              ],
+            ),
+            Divider(),
+          ],
+        ),
+      )
+    ],
   );
 }
